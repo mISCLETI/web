f9d52d66a729365aa1d386ac4247fb2893498191
Mikhail Ladygin <misc@rambler.ru>
2016-05-20 13:02:32 +0000

    Превед

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo rm /etc/gunicorn.d/*
-sudo ln -s /home/box/web/etc/hello.ру /etc/gunicorn.d/hello.ру
+#sudo rm /etc/gunicorn.d/*
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
