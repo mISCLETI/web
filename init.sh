7dea704c1d254e8cd3d252fd2dbd822fec36a15f
Mikhail Ladygin <misc@rambler.ru>
2016-05-20 13:20:41 +0000

    :app

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-#sudo rm /etc/gunicorn.d/*
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo rm /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello
 sudo /etc/init.d/gunicorn restart
