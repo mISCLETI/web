5ed0b9cc29885e145fd8da270f620df76091afbf
mISCLETI <misc@rambler.ru>
2016-05-20 15:08:03 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 sudo rm /etc/gunicorn.d/*
-sudo ln -sf /home /box/web/etc/hello.ру /etc/gunicorn.d/hello.ру
+sudo ln -s /home/box/web/etc/hello.ру /etc/gunicorn.d/hello.ру
 sudo /etc/init.d/gunicorn restart
