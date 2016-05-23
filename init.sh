cf4297b90050daec49d030c9036b0714b5e544f1
Mikhail Ladygin <misc@rambler.ru>
2016-05-23 07:41:42 +0000

    Добавил hello.py в usr/local/python2.7/lib

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 sudo rm /etc/gunicorn.d/test
-sudo rm /etc/gunicorn.d/hello.ру
-sudo ln -s /home/box/web/etc/hello.ру /etc/gunicorn.d/hello.ру
+sudo rm /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo cp /home/box/web/etc/hello.py /usr/local/lib/python2.7/hello.py
 sudo /etc/init.d/gunicorn restart
