4929d8f53b241d51a1b2789bf3840f34286196fc
Mikhail Ladygin <misc@rambler.ru>
2016-05-23 08:04:09 +0000

    Запускать нужно просто hello

--- init.sh
+++ init.sh
@@ -6,6 +6,8 @@ sudo /etc/init.d/nginx restart
 #sudo rm /etc/gunicorn.d/test
 sudo rm /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo cp /home/box/web/etc/hello.py /usr/local/lib/python2.7/hello.py
-sudo cp /home/box/web/etc/hello.py /usr/lib/python2.7/dist-packages/gunicorn/hello.py
+sudo rm /usr/local/lib/python2.7/hello.py
+sudo rm /usr/lib/python2.7/dist-packages/gunicorn/hello.py
+sudo ln -s /home/box/web/etc/hello.py /usr/local/lib/python2.7/hello.py
+sudo ln -s /home/box/web/etc/hello.py /usr/lib/python2.7/dist-packages/gunicorn/app/hello.py
 sudo /etc/init.d/gunicorn restart
