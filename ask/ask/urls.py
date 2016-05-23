bec7e038289247ee0e338368fb4a16f6b00f63a1
Mikhail Ladygin <misc@rambler.ru>
2016-05-23 07:16:28 +0000

    Поправил папочки

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = (
+      url(r'^$', 'qa.views.test', name='home'),
+      url(r'^login/$','qa.views.test'),
+      url(r'^signup/$','qa.views.test'),
+      url(r'^question/(\d+/)$', 'qa.views.test'),  #вместо 123 - произвольный ID
+      url(r'^ask/$', 'qa.views.test'),
+      url(r'^popular/$', 'qa.views.test'),
+      url(r'^new/$', 'qa.views.test'),
 )
