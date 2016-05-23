6b489048477f352c05bc6464c855d524029c4f1a
Mikhail Ladygin <misc@rambler.ru>
2016-05-23 07:57:20 +0000

    123

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo rm /etc/gunicorn.d/test
+#sudo rm /etc/gunicorn.d/test
 sudo rm /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo cp /home/box/web/etc/hello.py /usr/local/lib/python2.7/hello.py
